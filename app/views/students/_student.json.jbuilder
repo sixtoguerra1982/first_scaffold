4d8f8498397fbc06b00acdfa5aa9e23838e6a0c7
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-07-02 13:09:14 -0400

    first commit

---
+++ app/views/students/_student.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! student, :id, :name, :nivel, :created_at, :updated_at
+json.url student_url(student, format: :json)
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module CrudStudents
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # Time.zone = 'Santiago'
+    config.time_zone = 'Santiago'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
